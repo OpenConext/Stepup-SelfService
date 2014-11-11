3ff20be510fe9b36cec7e48b22f2f28b9865a45f
Reinier Kip <reinier@ibuildings.nl>
2014-11-11 12:01:24 +0100

    Create screen for entering Yubikey OTP.

---
+++ src/Surfnet/StepupSelfService/SelfServiceBundle/Controller/Registration/YubikeyController.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupSelfService\SelfServiceBundle\Controller\Registration;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class YubikeyController extends Controller
+{
+    /**
+     * @Template
+     */
+    public function verifyAction(Request $request)
+    {
+        $form = $this->createForm('ss_verify_yubikey_otp')->handleRequest($request);
+
+        if ($form->isValid()) {
+            return new Response('<h1>Not yet implemented</h1>', Response::HTTP_I_AM_A_TEAPOT);
+        }
+
+        return ['form' => $form->createView()];
+    }
+}
---
+++ src/Surfnet/StepupSelfService/SelfServiceBundle/Form/Type/VerifyYubikeyOtpType.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupSelfService\SelfServiceBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class VerifyYubikeyOtpType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+//        $builder->setAttribute('show_legend', true);
+        $builder
+            ->add('otp', 'text', array(
+                'label' => /** @Ignore */ false,
+                'required' => true,
+                'widget_addon_prepend' => array(
+                    'icon' => 'key'
+                ),
+                'attr' => array(
+                    'autofocus' => true,
+                    'autocomplete' => 'off',
+                )
+            ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Surfnet\StepupSelfService\SelfServiceBundle\Command\VerifyYubikeyOtpCommand',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'ss_verify_yubikey_otp';
+    }
+}
