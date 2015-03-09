c229e94ff76c64d6a8741e31fd1b5c2083a1beb9
DRvanR <dvrenterghem@ibuildings.nl>
2015-03-09 13:57:37 +0100

    Replaced own command with middleware client version
    - also updated ProofOfPossessionResult to use named constructors
    - added tests for ProofOfPossessionResult

---
+++ src/Surfnet/StepupSelfService/SelfServiceBundle/Tests/Service/YubikeySecondFactor/ProofOfPossessionResultTest.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupSelfService\SelfServiceBundle\Tests\Service\YubikeySecondFactor;
+
+use PHPUnit_Framework_TestCase as UnitTest;
+use Surfnet\StepupSelfService\SelfServiceBundle\Service\YubikeySecondFactor\ProofOfPossessionResult;
+
+class ProofOfPossessionResultTest extends UnitTest
+{
+    /**
+     * @test
+     * @group yubikey
+     */
+    public function an_invalid_otp_gives_an_unsuccessful_result_without_second_factor_id()
+    {
+        $result = ProofOfPossessionResult::invalidOtp();
+
+        $this->assertFalse($result->isSuccessful());
+        $this->assertNull($result->getSecondFactorId());
+        $this->assertTrue($result->isOtpInvalid());
+
+        $this->assertFalse($result->didOtpVerificationFail());
+        $this->assertFalse($result->didProofOfPossessionCommandFail());
+    }
+
+    /**
+     * @test
+     * @group yubikey
+     */
+    public function otp_verification_failure_gives_an_unsuccessful_result_without_second_factor_id()
+    {
+        $result = ProofOfPossessionResult::otpVerificationFailed();
+
+        $this->assertFalse($result->isSuccessful());
+        $this->assertNull($result->getSecondFactorId());
+        $this->assertTrue($result->didOtpVerificationFail());
+
+        $this->assertFalse($result->isOtpInvalid());
+        $this->assertFalse($result->didProofOfPossessionCommandFail());
+    }
+
+    /**
+     * @test
+     * @group yubikey
+     */
+    public function a_failed_proof_of_possession_command_gives_an_unsuccessful_result_without_second_factor_id()
+    {
+        $result = ProofOfPossessionResult::proofOfPossessionCommandFailed();
+
+        $this->assertFalse($result->isSuccessful());
+        $this->assertNull($result->getSecondFactorId());
+        $this->assertTrue($result->didProofOfPossessionCommandFail());
+
+        $this->assertFalse($result->didOtpVerificationFail());
+        $this->assertFalse($result->isOtpInvalid());
+    }
+
+    /**
+     * @test
+     * @group yubikey
+     */
+    public function when_the_second_factor_has_been_created_the_result_is_successful_with_second_factor_id()
+    {
+        // generated once using \Rhumsaa\Uuid\Uuid::uuid4()
+        $uuidV4 = '2daf34c1-22fe-4399-8db9-42492f600cce';
+
+        $result = ProofOfPossessionResult::secondFactorCreated($uuidV4);
+
+        $this->assertTrue($result->isSuccessful());
+        $this->assertEquals(
+            $uuidV4,
+            $result->getSecondFactorId(),
+            'The given SecondFactorId should be returned upon request'
+        );
+
+        $this->assertFalse($result->isOtpInvalid());
+        $this->assertFalse($result->didOtpVerificationFail());
+        $this->assertFalse($result->didProofOfPossessionCommandFail());
+    }
+}
