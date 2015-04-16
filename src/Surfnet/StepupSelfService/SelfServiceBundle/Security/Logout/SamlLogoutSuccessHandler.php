ff7fdefbe133530eee2b763206038debed8f06d5
Reinier Kip <reinier@ibuildings.nl>
2015-04-16 14:36:22 +0200

    Display sign-out button when user is authenticated

---
+++ src/Surfnet/StepupSelfService/SelfServiceBundle/Security/Logout/SamlLogoutSuccessHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupSelfService\SelfServiceBundle\Security\Logout;
+
+use Surfnet\StepupSelfService\SelfServiceBundle\Security\Authentication\SessionHandler;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\Security\Http\Logout\DefaultLogoutSuccessHandler;
+
+final class SamlLogoutSuccessHandler extends DefaultLogoutSuccessHandler
+{
+    /**
+     * @var \Surfnet\StepupSelfService\SelfServiceBundle\Security\Authentication\SessionHandler
+     */
+    private $sessionHandler;
+
+    public function __construct(SessionHandler $sessionHandler, HttpUtils $httpUtils)
+    {
+        parent::__construct($httpUtils);
+
+        $this->sessionHandler = $sessionHandler;
+    }
+
+    public function onLogoutSuccess(Request $request)
+    {
+        $this->sessionHandler->clearToken();
+
+        return parent::onLogoutSuccess($request);
+    }
+}
