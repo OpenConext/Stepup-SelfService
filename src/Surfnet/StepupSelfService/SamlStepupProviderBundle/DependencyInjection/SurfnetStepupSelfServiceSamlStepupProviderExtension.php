f210b093ccfcc624cbaa6ccf6ae791914ac814cb
Michiel Kodde <mkodde@ibuildings.nl>
2017-05-29 15:06:56 +0200

    Load the Gssp configuration into the Bundle

--- src/Surfnet/StepupSelfService/SamlStepupProviderBundle/DependencyInjection/SurfnetStepupSelfServiceSamlStepupProviderExtension.php
+++ src/Surfnet/StepupSelfService/SamlStepupProviderBundle/DependencyInjection/SurfnetStepupSelfServiceSamlStepupProviderExtension.php
@@ -35,7 +35,6 @@ class SurfnetStepupSelfServiceSamlStepupProviderExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
@@ -56,6 +55,7 @@ class SurfnetStepupSelfServiceSamlStepupProviderExtension extends Extension
         array $routes,
         ContainerBuilder $container
     ) {
+
         if ($container->has('gssp.provider.' . $provider)) {
             throw new InvalidConfigurationException(sprintf('Cannot create the same provider "%s" twice', $provider));
         }
@@ -79,6 +79,17 @@ class SurfnetStepupSelfServiceSamlStepupProviderExtension extends Extension
 
         $providerDefinition->setPublic(false);
         $container->setDefinition('gssp.provider.' . $provider, $providerDefinition);
+
+        $viewConfigDefinition = new Definition('Surfnet\StepupSelfService\SamlStepupProviderBundle\Provider\ViewConfig', [
+            $configuration['view_config']['loa'],
+            $configuration['view_config']['logo'],
+            $configuration['view_config']['alt'],
+            $configuration['view_config']['title'],
+            $configuration['view_config']['description'],
+            $configuration['view_config']['button_use'],
+        ]);
+
+        $container->setDefinition('gssp.view_config.' . $provider, $viewConfigDefinition);
 
         $container
             ->getDefinition('gssp.provider_repository')
