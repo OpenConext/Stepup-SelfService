7c89c5b3e8733816c10dcfee2e38ca5afffe7472
Michiel Kodde <mkodde@ibuildings.nl>
2018-11-01 09:44:06 +0100

    Set actor id on the verified second factor query
    The is a requirement set in the middleware API, used for RA management
    related actions. We just pass the users identity id as the actor id.
    Which is correct behaviour.

--- src/Surfnet/StepupSelfService/SelfServiceBundle/Service/SecondFactorService.php
+++ src/Surfnet/StepupSelfService/SelfServiceBundle/Service/SecondFactorService.php
@@ -183,6 +183,8 @@ class SecondFactorService
     {
         $query = new VerifiedSecondFactorSearchQuery();
         $query->setIdentityId($identityId);
+        // In self service the actor equals the identity of the user.
+        $query->setActorId($identityId);
         $query->setActorInstitution($actorInstitution);
         $query->setInstitution($actorInstitution);
         // Actor and identity are equal in SelfService.
