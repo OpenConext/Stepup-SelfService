661cb7b8179d9db3dcc97e659da0bd6f19ff830a
Martin Roest <roest.martin@gmail.com>
2020-11-17 14:03:09 +0100

    Update yaml files

--- config/bundles.php
+++ config/bundles.php
@@ -12,9 +12,9 @@ return [
     Surfnet\SamlBundle\SurfnetSamlBundle::class => ['all' => true],
     Surfnet\StepupSelfService\SelfServiceBundle\SurfnetStepupSelfServiceSelfServiceBundle::class => ['all' => true],
     Surfnet\StepupSelfService\SamlStepupProviderBundle\SurfnetStepupSelfServiceSamlStepupProviderBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
