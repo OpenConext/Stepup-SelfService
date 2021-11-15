fe7bbb6299dbc8839fab725c4ea521c78ca0b570
Michiel Kodde <mkodde@ibuildings.nl>
2021-11-15 10:47:08 +0100

    Update bundles.php for smoketest environment

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'smoketest' => true],
 ];
