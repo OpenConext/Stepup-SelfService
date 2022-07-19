5e06d5763f70b3f220a20ac33d93c7a69f4add5e
Michiel Kodde <mkodde@ibuildings.nl>
2022-07-19 11:10:54 +0200

    Show loa star rating
    This is a bit of a boy scout interlude. Added to this PR to make my life
    easier. Distinguishing between token types and their vetting type became
    increasingly difficult during testing.

---
+++ src/Surfnet/StepupSelfService/SelfServiceBundle/Service/VettingTypeService.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupSelfService\SelfServiceBundle\Service;
+
+use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\RegistrationAuthorityCredentialsCollection;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Service\RaService as ApiRaService;
+
+class RaService
+{
+    /**
+     * @var ApiRaService
+     */
+    private $api;
+
+    public function __construct(ApiRaService $raService)
+    {
+        $this->api = $raService;
+    }
+
+    /**
+     * @param string $institution
+     * @return RegistrationAuthorityCredentialsCollection
+     */
+    public function listRas($institution)
+    {
+        return $this->api->listRas($institution);
+    }
+
+    public function listRasWithoutRaas($institution)
+    {
+        $allRas = $this->api->listRas($institution);
+
+        $rasWithoutRaas = [];
+        foreach ($allRas->getElements() as $ra) {
+            if (!$ra->isRaa) {
+                $rasWithoutRaas[] = $ra;
+            }
+        }
+
+        // All RAs and RAAs are fetched, so this can safely be returned (no pagination is used here)
+        return new RegistrationAuthorityCredentialsCollection(
+            $rasWithoutRaas,
+            count($rasWithoutRaas),
+            $allRas->getItemsPerPage(),
+            $allRas->getCurrentPage()
+        );
+    }
+}
