2f16accb6952bee3213d8d81357f29c1776884d3
Michiel Kodde <mkodde@ibuildings.nl>
2023-11-02 13:30:55 +0100

    Update the front controller to match the SF6 way

--- public/index.php
+++ public/index.php
@@ -1,40 +1,9 @@
 <?php
 
-use App\Kernel;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
+use Surfnet\StepupSelfService\Kernel;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    // see fix in Sulu https://github.com/sulu/sulu/pull/5729/files
-    Request::setTrustedProxies(
-        \explode(',', $trustedProxies),
-        Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO
-    );
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-// Support for behat test from stepup-deploy, when the `testcookie` and GuzzleHttp user agent
-// are present and app_env is not prod, then engage smoketest mode.
-if ($_SERVER['APP_ENV'] !== 'prod' &&
-    isset($_COOKIE['testcookie']) &&
-    str_contains($_SERVER['HTTP_USER_AGENT'], 'GuzzleHttp')
-) {
-    $_SERVER['APP_ENV'] = 'smoketest';
-    $_SERVER['APP_DEBUG'] = true;
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
