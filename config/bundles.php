399e08e36b70fc1a3a7450cb9c1f2fee9ee4215a
Paul Rijke <paul@ibuildings.nl>
2023-11-30 15:04:29 +0100

    Remove sensio_framework_extra bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'smoketest' => true],
     JMS\TranslationBundle\JMSTranslationBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
