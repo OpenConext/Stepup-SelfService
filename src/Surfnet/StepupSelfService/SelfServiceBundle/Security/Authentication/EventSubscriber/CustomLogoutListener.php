b07f352d22304a4f37bfe34eb935967c62781fba
Paul Rijke <paul@ibuildings.nl>
2023-11-30 16:06:39 +0100

    Replaced deprecated handler to eventsubscriber

---
+++ src/Surfnet/StepupSelfService/SelfServiceBundle/Security/Authentication/EventSubscriber/CustomLogoutListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Surfnet\StepupSelfService\SelfServiceBundle\Security\Authentication\EventSubscriber;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+readonly class CustomLogoutListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            LogoutEvent::class => 'onLogout',
+        ];
+    }
+
+    public function __construct(
+        private TokenStorageInterface $tokenStorage,
+        private array $logoutRedirectUrl,
+    ) {
+    }
+    public function onLogout(): RedirectResponse
+    {
+        $token    = $this->tokenStorage->getToken();
+        $identity = $token->getUser();
+
+        return new RedirectResponse($this->logoutRedirectUrl[$identity->preferredLocale]);
+    }
+
+}
