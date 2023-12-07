815e1447ec7b2664179853a84ddffd883ba2e82b
Paul Rijke <paul@ibuildings.nl>
2023-12-07 14:37:00 +0100

    Moved to parameters instead of .env
    According to the decision made by the team at November 7th, 2023

---
+++ config/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+use Symfony\Component\Yaml\Yaml;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+$parametersPath = dirname(__DIR__).'/config/openconext/parameters.yaml';
+$parameters = Yaml::parseFile($parametersPath);
+$parameters = $parameters['parameters'];
+$requiredParameters = ['app_env', 'app_debug', 'app_secret'];
+
+// Test if required parameters are set
+if (0 !== count(array_diff($requiredParameters, array_keys($parameters)))) {
+    throw new RuntimeException(sprintf(
+        'Required parameters are not configured, required params are: %s, configure them in %s',
+        implode(', ', $requiredParameters),
+        $parametersPath
+    ));
+}
+
+$_SERVER['APP_ENV'] = $parameters['app_env'];
+$_SERVER['APP_DEBUG'] = $parameters['app_debug'];
+$_SERVER['APP_SECRET'] = $parameters['app_secret'];
+
+filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
