8b493255504abc8a7d3cf1bf46a82cdd1acd6a39
Paul Rijke <paul@ibuildings.nl>
2023-12-07 14:37:00 +0100

    Moved to parameters instead of .env
    According to the decision made by the team at November 7th, 2023

--- public/index.php
+++ public/index.php
@@ -18,6 +18,7 @@
 use Surfnet\StepupSelfService\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/config/bootstrap.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
